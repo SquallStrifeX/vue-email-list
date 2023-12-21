13805d645b877cf540d83fbab9ad51a16d7408c8
SquallStrifeX <squallstrifex@gmail.com>
2023-12-21 02:45:55 +0100

    fixed end exercise

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ createApp({
       }
     },
 created(){
-  for(i=0; i<10; i++){
+  for(i=0; i<9; i++){
   axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (response) => {
 
     this.randomMail.push(response.data.response)
@@ -16,5 +16,12 @@ created(){
 
 
   })}
+  axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (response) => {
+
+    this.randomMail.push(response.data.response)
+    console.log(this.randomMail)
+
+
+  })
 }
 }).mount('#app')
